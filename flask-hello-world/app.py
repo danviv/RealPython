5a0991e2b3f61dda7932a3ef5ea5c6c4d3990baf
Vi <Vivekdan@gmail.com>
2014-05-11 19:04:26 +0100

    flask-hello-world_dynamic_debugmode

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -2,14 +2,22 @@
 
 from flask import Flask
 
+#create the app object
 app = Flask(__name__)
 
+#error handling
+app.config["DEBUG"] = True
+
+#decorators - static route
 @app.route("/")
 @app.route("/hello")
-
-
 def hello_world():
-	return "Hello World!"
+	return "Hello World!?!?!?!"
+
+#decorators - dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
 
 if __name__ == "__main__":
 	app.run()
