26c4f81dc3b0a0cc78c129e0ca80a6988ee74a7b
Vi <Vivekdan@gmail.com>
2014-06-01 15:29:06 +0100

    web2py movie-suggestor'

---
+++ web2py/start/applications/pulse/controllers/test.py
@@ -0,0 +1,7 @@
+# coding: utf8
+# try something like
+def index(): 
+    return dict(message="hello from test.py")
+
+def data():
+    return (int(request.vars.key)+10)
--- web2py/start/parameters_8000.py
+++ web2py/start/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$bd6d3384ec268252$0fe9c7e913645f5cdae2a26b9c16f53eb52ddd2b"
+password="pbkdf2(1000,20,sha512)$b3024a2a81df5fa3$0ff7ea8a17127fb461f2a8258f8c8a3f5c4638c3"
