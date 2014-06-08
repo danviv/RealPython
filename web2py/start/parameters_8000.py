fb58084fab8dda2b9ad1a34b4cab3173fc48a502
Vi <Vivekdan@gmail.com>
2014-06-08 17:25:49 +0100

    web2py blog

--- web2py/start/parameters_8000.py
+++ web2py/start/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b3024a2a81df5fa3$0ff7ea8a17127fb461f2a8258f8c8a3f5c4638c3"
+password="pbkdf2(1000,20,sha512)$b3cf07befcd0b991$e3c7b3e9a9d4dbb96483cbd07a1fdd3d9caee416"
